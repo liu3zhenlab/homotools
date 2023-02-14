2410f979fbb5e780b42f0b56a4c4b296d354d432
liu3zhenlab <liu3zhen@ksu.edu>
2023-02-13 20:16:43 -0600

    update

--- lib/gffadjust.pm
+++ lib/gffadjust.pm
@@ -20,8 +20,8 @@ sub gffadjust {
 	use strict;
 	use warnings;
 	
-	my $feature_height = 0.03;
-	my $feature_color = "gray20";
+	my $feature_height = 0.025;
+	my $feature_color = "aquamarine3";
 
 	my ($outpath, $gff, $start, $seqname) = @_;
 	my %entries;
