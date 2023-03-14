ff9cfeb66352c50122448601876685ba1838d822
liu3zhenlab <liu3zhen@ksu.edu>
2023-03-14 10:28:20 -0500

    update

--- utils/homograph_msa.plot.R
+++ utils/homograph_msa.plot.R
@@ -1,16 +1,21 @@
 # R for plot Nucmer alignments
 args <- commandArgs(trailingOnly=T)
 blocktype <- args[1] # blocktying input
-genebed <- args[2] # gene BED
+adjust_genebed <- args[2] # gene BED with coordinates adjusted
 title <- args[3] # figure title
 outpdf <- args[4] # PDF output file
 
-blocktype <- "hgout/hgout.3.blocktyping"
-genebed <- "../0_gtfbed/GRMZM2G171650_T01.adjusted.bed"
-title <- "figure"
-outpdf <- "out.pdf"
+cat(blocktype, "\n")
+cat(adjust_genebed, "\n")
+cat(title, "\n")
+cat(outpdf, "\n")
 
 #setwd("/homes/liu3zhen/scripts2/homotools/homograph_dev/test")
+#blocktype <- "hgout/hgout.3.blocktyping"
+#genebed <- "../0_gtfbed/GRMZM2G171650_T01.adjusted.bed"
+#adjust_genebed <- "hgout/hgout.4.gene.bed"
+#title <- "haplotype view"
+#outpdf <- "out.pdf"
 
 #############################################################
 # module
@@ -101,62 +106,134 @@ left_margin <- round(taxa_names_maxlen / 5) + 1
 xmax <- max(d[, 3])
 taxa <- unique(d[, 1])
 ymax <- length(taxa)
-if (genebed != "NULL") {
-  ymax <- ymax + 4
+
+######################################################
+### BED file
+######################################################
+is_bed_plot <- F
+if (file.exists(adjust_genebed)) {
+  file_info <- file.info(adjust_genebed)
+  if (file_info$size > 0) {
+    is_bed_plot <- T
+    ymax <- ymax + 2
+  }
 }
-yheight <- 2 + (ymax - 2) * 0.2
+
+highest_polymorphisms <- max(d[, 6])
+if (highest_polymorphisms >= 1) {
+  ymax <- ymax + 3
+}
+
+yheight <- 2 + (ymax - 2) * 0.15
 
 # plot setting
 # par(mar=c(2.2, 0.5, 1, 0.5), mgp=c(1.5,0.1,0))
 pdf(outpdf, width=5, height=yheight)
-par(mar=c(left_margin, 3, 2.5, 0))
+
+par(mar=c(left_margin, 3.5, 2.2, 0.5), mgp=c(1, 0.65, 0))
 plot(NULL, NULL, xlim=c(0, xmax), ylim=c(1, ymax),
      frame.plot=F, xlab="", ylab="",
      xaxt="n", yaxt="n")
 
-# plot gene structure
-if (genebed != "NULL") {
-  bed <- read.delim(genebed, header=F, comment.char="#")
-  bed_min <- min(bed[, 2])
-  bed_max <- max(bed[, 3])
-  adjust_height <- height*max(1, ymax/20)
-  x_adj <- xmax/2 
-  ycenter <- ymax
-  lines(c(bed_min, bed_max), rep(ycenter, 2), col="gray50", lwd=1)
-  if (nrow(bed)>0) {
-    for (i in 1:nrow(bed)) {
-      color <- bed[i, 7]
+### plot original gene structure
+if (is_bed_plot) {
+  max_adjust_height <- 0
+  
+  ### plot coordinate-adjusted gene structure
+  newbed <- read.delim(adjust_genebed, header=F, comment.char="#", stringsAsFactors=F)
+  is_underscore <- sum(newbed[, 4] == "UNDERSCORE")>0
+  if (is_underscore) {
+    underscore <- newbed[newbed[, 4] == "UNDERSCORE", ]
+    newbed <- newbed[newbed[, 4] != "UNDERSCORE", ]
+  }
+  seqname <- unique(newbed[, 1])[1]
+  newbed_min <- min(newbed[, 2])
+  newbed_max <- max(newbed[, 3])
+  ycenter_2 <- ymax
+  lines(c(newbed_min, newbed_max), rep(ycenter_2, 2), col="gray50", lwd=0.6)
+  if (nrow(newbed)>0) {
+    for (i in 1:nrow(newbed)) {
+      color <- newbed[i, 7]
       if (!isColor(color)) {
         color <- "grey"
       }
-      height <- bed[i, 5]
-      rect(bed[i, 2] + 1, ycenter-adjust_height/2,
-           bed[i, 3], ycenter+adjust_height/2,
-           col=color, border=color)
+      height <- as.numeric(newbed[i, 5])
+      adjust_height <- height*max(1, ymax/15)
+      max_adjust_height <- max(max_adjust_height, adjust_height)
+      rect(newbed[i, 2] + 1, ycenter_2 - adjust_height/2,
+           newbed[i, 3], ycenter_2 + adjust_height/2,
+           col=color, border=NA)
     }
-  }  
+  }
+  text(newbed_min, ycenter_2, adj=2, labels=seqname, cex=0.6, xpd=T)
+  
+  ### underscore plotting
+  if (is_underscore) {
+    for (i in 1:nrow(underscore)) {
+      lines(underscore[i, 2:3], rep(ycenter_2 - max_adjust_height, 2),
+            lwd=1, col=underscore[i, 7], lend=1)
+    }
+  }
 }
 
+######################################################
 # x-axis
+######################################################
 xaxis <- smartaxis(xmax)
 axis(1, at=xaxis[[1]], labels=xaxis[[2]], tick=T, col="gray50", cex.axis=0.8)
-mtext(paste0("position (", xaxis[[3]], ")"), side=1, line=2)
+mtext(paste0("position (", xaxis[[3]], ")"), side=1, line=1.8)
 
+######################################################
 # title
-mtext(title, side=3, line=1.2)
+######################################################
+mtext(title, side=3, line=1)
 
-
+######################################################
+### num of polymorphisms
+######################################################
+if (highest_polymorphisms >= 1) {
+  ypos <- length(taxa) + 1.5
+  log10max <- round(log10(highest_polymorphisms + 1), 0)
+  pscale <- 2 / log10max
+  hlines_val <- seq(0, log10max, by=1)
+  lines_pos = ypos + hlines_val*pscale
+  for (i in 1:length(lines_pos)) {
+    lines(c(-xmax/100, xmax), rep(lines_pos[i], 2), lwd=0.5, lty=2, col="gray80")
+    text(-xmax/100, lines_pos[i], adj=2, labels=hlines_val[i], las=1, cex=0.6)
+  }
+  #axis(2, at=c(ypos, ypos+log10max*pscale), labels=c(0, log10max),
+  #     tick=T, col="gray50", cex.axis=0.6, las=1)
+  text(-xmax/85, mean(lines_pos), labels="log10", adj=2, las=1, cex=0.7, xpd=T)
+  #hlines_val <- seq(0, log10max, by=1)
+  #abline(h=ypos + hlines_val*pscale, lwd=0.1, lty=2)
+  # nums for plotting
+  uniqseg <- !duplicated(paste(d[, 4], d[, 5], d[, 6]))
+  if (sum(uniqseg)>0) {
+    uniqseq_numpoly <- d[uniqseg, c(4,5,6)]
+    for (i in 1:nrow(uniqseq_numpoly)) {
+      plot_val <- log10(uniqseq_numpoly[i, 3] + 1)
+      rect(uniqseq_numpoly[i, 1], ypos,
+           uniqseq_numpoly[i, 2], ypos+plot_val*pscale,
+           col="palevioletred", border=NA)
+    }
+  }
+}
+  
+######################################################
 # backgrounp of alignment regions (alignment segments)
+######################################################
 row <- 0
 for (etaxon in taxa) {
   alnseg <- paste(d[, 1], d[, 2], d[, 3])
   taxon_data <- d[!duplicated(alnseg) & d[, 1] == etaxon, 1:3] 
   row <- row + 1
   apply(taxon_data[, 2:3], 1, block_plot, yrange=c(row-0.35, row+0.35), singlecol="lightblue")
-  text(-xmax/50, row, adj=1, labels=etaxon, xpd=T, cex=0.6)
+  text(-xmax/80, row, adj=1, labels=etaxon, xpd=T, cex=0.6)
 }
 
+######################################################
 # block and color-coded difference from the consensus
+######################################################
 row <- 0
 for (etaxon in taxa) {
   block_taxon_data <- d[d[, 1] == etaxon, 4:7] 
@@ -165,5 +242,5 @@ for (etaxon in taxa) {
         lowcol="cornsilk1", highcol="darkorange")
 }
 
-# dev.off()
+dev.off()
 
